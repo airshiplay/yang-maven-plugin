92ada23cacf34246e3afed7b63f52141d0486b7b
airlenet <airlenet@163.com>
2021-02-02 09:12:18 +0800

    update tailf action

--- yang-compiler/translator/src/main/java/com/airlenet/yang/compiler/translator/tojava/javamodel/YangJavaListTranslator.java
+++ yang-compiler/translator/src/main/java/com/airlenet/yang/compiler/translator/tojava/javamodel/YangJavaListTranslator.java
@@ -348,14 +348,14 @@ public class YangJavaListTranslator
             YangNode child = this.getChild();
             while (child != null) {
                 if (child instanceof YangJavaUnion || child instanceof YangJavaUses || child instanceof YangJavaGrouping || child instanceof YangJavaEnumeration
-                        || child instanceof YangJavaAction || child instanceof YangJavaTailfAction) {
+                        || child instanceof YangJavaAction ) {
 
                 } else if (child instanceof YangJavaList) {
 //                JNCCodeUtil.yangNodeMethond(javaClass, child);
                     JNCCodeUtil.yangJavaListMethod(javaClass, (YangJavaList) child);
-                } else if (child instanceof YangJavaContainer) {
+                } else if (child instanceof YangJavaContainer || child instanceof YangJavaTailfAction) {
                     JNCCodeUtil.yangNodeMethond(javaClass, child, true);
-                    JNCCodeUtil.yangJavaContainerMethod(javaClass, (YangJavaContainer) child);
+                    JNCCodeUtil.yangJavaContainerMethod(javaClass, (YangNode) child);
                 } else if (child instanceof YangJavaChoice) {
                     YangNode childChild = child.getChild();
                     while (childChild != null) {
